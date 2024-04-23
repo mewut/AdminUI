27d9a125b86e57ee15658024844969315735491a
Name <shi-666@yandex.ru>
2024-04-24 01:25:53 +0300

    init

---
+++ script.js
@@ -0,0 +1,10 @@
+// document.addEventListener('DOMContentLoaded', ()) => {
+//     const menuItems = document.querySelectorAll('.menu__item');
+//     menuItems.forEach((item) => {
+//       item.addEventListener('click', (event) => {
+//         event.preventDefault();
+//         const targetId = event.target.getAttribute('href');
+//         history.pushState(null, null, targetId);
+//       });
+//     });
+// }
