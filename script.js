74086c94fd0c4f5e13353b1e2f3c5b1ba9b070aa
Name <shi-666@yandex.ru>
2024-04-25 23:42:36 +0300

    json parse

--- script.js
+++ script.js
@@ -1,10 +1,28 @@
-// document.addEventListener('DOMContentLoaded', ()) => {
-//     const menuItems = document.querySelectorAll('.menu__item');
-//     menuItems.forEach((item) => {
-//       item.addEventListener('click', (event) => {
-//         event.preventDefault();
-//         const targetId = event.target.getAttribute('href');
-//         history.pushState(null, null, targetId);
-//       });
-//     });
-// }
+document.querySelectorAll('.menu__link').forEach((link, index) => {
+    link.addEventListener('click', () => {
+        document.querySelectorAll('.content-1, .content-2, .content-3, .content-4, .content-5').forEach(content => {
+            content.style.display = 'none';
+        });
+        document.querySelector(`.content-${index + 1}`).style.display = 'block';
+    });
+});
+
+
+
+  fetch('data.json')
+  .then(response => response.json())
+  .then(data => {
+    Object.keys(data).forEach(key => {
+      const contentElement = document.querySelector(`.${key}`);
+      const titleElement = document.createElement('h1');
+      titleElement.textContent = data[key].title;
+      const textElement = document.createElement('p');
+      textElement.textContent = data[key].text;
+
+      contentElement.appendChild(titleElement);
+      contentElement.appendChild(textElement);
+    });
+  })
+  .catch(error => {
+    console.error('Ошибка при загрузке данных:', error);
+  });
